89795ebf03db1051c5c44d0dc132b24364c9e700
chouglesaud <saudchougle.sc@gmail.com>
2019-09-22 13:02:13 +0530

    ssl

--- server.js
+++ server.js
@@ -102,9 +102,13 @@
         // SEO 
         // robot.txt
         // sitemap.xml
- 
+        app.get("/.well-known/pki-validation/7466059DDBB0B1F91FD0479C0EED3B2A.txt",(req,res)=>{
+            let txt = "F25ADB64FFB376395D79E51DAF02D3B1DFFEEBFFB89FC9658F791AEA8C2F4B16 comodoca.com 5d8722414b6c5"
+            res.header('Content-Type', 'text/txt');
+            res.send(txt)
+        })
         
-        app.get("/robot.txt",(req,res)=>{
+        app.get("/robot.tx                                                                                                                                                                  t",(req,res)=>{
             let txt = `
             User-agent: Mediapartners-Google\n
             Allow: /\n
