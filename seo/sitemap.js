1531f4718caa72d8ad306c5b6a5d452143a9c5c6
chouglesaud <saudchougle.sc@gmail.com>
2019-09-25 18:51:20 +0530

    sitemap

--- seo/sitemap.js
+++ seo/sitemap.js
@@ -8,38 +8,38 @@ module.exports = Recent.find({}).then(async(found)=>{
       await found.forEach((data)=>{
            xml +=`
            <url>\n
-               <loc>http://codebond.co${data.url}</loc>\n
+               <loc>https://codebond.co${data.url}</loc>\n
                <priority>0.6</priority>\n
            </url>\n
            `
        })
        xml += `
        <url>\n
-            <loc>http://codebond.co/</loc>\n
+            <loc>https://codebond.co/</loc>\n
             <priority>0.5</priority>\n
        </url>\n
        <url>\n
-       <loc>http://codebond.co/tutorial/javascript</loc>\n
+       <loc>https://codebond.co/tutorial/javascript</loc>\n
        <priority>0.3</priority>\n
        </url>\n  
        <url>\n
-       <loc>http://codebond.co/tutorial/nodejs</loc>\n
+       <loc>https://codebond.co/tutorial/nodejs</loc>\n
        <priority>0.3</priority>\n
        </url>\n  
        <url>\n
-       <loc>http://codebond.co/tutorial/reactjs</loc>\n
+       <loc>https://codebond.co/tutorial/reactjs</loc>\n
        <priority>0.3</priority>\n
        </url>\n  
        <url>\n
-       <loc>http://codebond.co/tutorial/npm</loc>\n
+       <loc>https://codebond.co/tutorial/npm</loc>\n
        <priority>0.3</priority>\n
        </url>\n  
        <url>\n
-       <loc>http://codebond.co/tutorial/css</loc>\n
+       <loc>https://codebond.co/tutorial/css</loc>\n
        <priority>0.3</priority>\n
        </url>\n  
        <url>\n
-       <loc>http://codebond.co/tutorial/other</loc>\n
+       <loc>https://codebond.co/tutorial/other</loc>\n
        <priority>0.3</priority>\n
        </url>\n  
        </urlset>
