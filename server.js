58563b65135931633ff367110b50caf4fac38232
Chougle-saud <saudchougle.sc@gmail.com>
2019-12-25 20:24:17 +0530

    editor.js

--- server.js
+++ server.js
@@ -199,10 +199,7 @@ app.post(
     
     let result = await cloudinary.v2.uploader.upload(req.body.file)
     let splitResult = result.secure_url.split("/");
-    let newUrl = `https://res.cloudinary.com/codebond/image/upload/${
-      splitResult[6]
-    }/${splitResult[7]}`;
-    let isHosted = await Update.image_hosting(req.user.id, { gallery: newUrl });
+    let newUrl = `https://res.cloudinary.com/codebond/image/upload/${splitResult[6]}/${splitResult[7]}`;
     if (isHosted) {
       res.json({url: newUrl})
     }
