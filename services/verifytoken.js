cc9705d6dba28faac997b9fccd1c8fd81407a070
Chougle-saud <saudchougle.sc@gmail.com>
2020-01-23 09:57:48 +0530

    1-vuejs added.2-global post publish added 5

--- services/verifytoken.js
+++ services/verifytoken.js
@@ -1,24 +1,24 @@
-const jwt = require("jsonwebtoken")
+const jwt = require('jsonwebtoken')
 
-module.exports =  function(req,res,next){
-    const token = req.params.token
-    if(token){
-        jwt.verify(token,process.env.jwtSecret,(err,done)=>{
-            if(err){
-                let error = {
-                    name      : 404,
-                    firstnum  : 4,
-                    secondnum : 0,
-                    thirdnum  : 4,
-                    firstword : "webpage",
-                    secondword: "not",
-                    thirdword : "found"
-                }
-                return res.render("error",{user: req.user,error,nouser: false})
-            }
-            next()
-        })
-        
-    }
-   
+module.exports = function(req, res, next) {
+	const token = req.params.token
+	if (token) {
+		jwt.verify(token, process.env.jwtSecret, (err, done) => {
+			if (err) {
+				console.log('here')
+
+				let error = {
+					name: 404,
+					firstnum: 4,
+					secondnum: 0,
+					thirdnum: 4,
+					firstword: 'webpage',
+					secondword: 'not',
+					thirdword: 'found'
+				}
+				return res.render('error', { user: req.user, error, nouser: false })
+			}
+			next()
+		})
+	}
 }
